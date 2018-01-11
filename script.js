3230b2f3f82ab8eff385b54d33b9a7cfdf4e30c0
Sudhakar Shah <sudhakarshah2013@gmail.com>
2018-01-11 22:43:05 +0530

    comments added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var taskList = [];
-var sec=[];
+var taskList = [];     //  list of task name
+var sec=[];            // list of duration of each task
 var taskState=[];  // 1: on     0: off      -1 : task complete
-var count=0;
+var count=0;          // number of tasks
 
 function createTask(form){
     var p = document.getElementsByTagName("body");
@@ -10,6 +10,21 @@ function createTask(form){
     startTask(form);
 }
 
+
+function startTask(form){
+  document.getElementById("startButton").disabled=true;
+  var task = form.inputbox.value;
+  form.inputbox.value = "";
+  taskList.push(task);
+  sec.push(0);
+  taskState.push(1);
+  var h='<div id="task'+count+'"><div class="alignleft" id="name'+count+'">'+taskList[taskList.length-1]+'</div><div class="alignright" ><span class="time">00 secs</span><span id="'+count+'"><img  src="images/pause.png"  id="playPause"  onClick="playPause(this.parentNode.id)"><img src="images/tick.png" id="finish" onClick="taskFinish(this.parentNode.id)"></span></div></div>'
+  h='<br>'+h;            //adding break between two tasks
+  $('#box').append(h);   // appending new task div
+  count++;
+}
+
+// function to disable the start button when textbox is empty
 function keyUp(event){
   var i=document.getElementsByClassName("input")[0];
   console.log(i.value);
@@ -19,20 +34,8 @@ function keyUp(event){
       document.getElementById("startButton").disabled=false;
 }
 
-function startTask(form){
-  document.getElementById("startButton").disabled=true;
-  var task = form.inputbox.value;
-  form.inputbox.value = "";
-  taskList.push(task);
-  sec.push(0);
-  taskState.push(1);
-  var h='<div id="task'+count+'"><div class="alignleft" id="name'+count+'">'+taskList[taskList.length-1]+'</div><div class="alignright" ><span class="time">00 secs</span><span id="'+count+'"><img  src="images/pause.png"  id="playPause"  onClick="pause(this.parentNode.id)"><img src="images/tick.png" id="finish" onClick="taskFinish(this.parentNode.id)"></span></div></div>'
-  h='<br>'+h;
-  $('#box').append(h);
-  count++;
-}
-
-function pause(id){
+// changing play pause state and button image
+function playPause(id){
   if (taskState[id]==0)
   {
     $('#'+id).children('#playPause').attr('src', 'images/pause.png');
@@ -48,21 +51,23 @@ function pad ( val ) {
   return val > 9 ? val : "0" + val;
 }
 
+
 function taskFinish(id){
   taskState[id]=-1;
-  $("#"+id).hide();
+  $("#"+id).hide();           // hiding the buttons when task finishes
   var elem = document.createElement("img");
   elem.src = 'images/done.png';
   elem.setAttribute("height", "15");
   elem.setAttribute("width", "15");
   document.getElementById("name"+id).appendChild(elem);
-  $('#task'+id).css('background-color', '#C0B4B4');
+  $('#task'+id).css('background-color', '#C0B4B4');             // changing background color of task div
 }
 
 function setTime()
 {
     for(i=0; i<taskList.length; i++)
     {
+      // if task state is active then we increase the time and display it
       if (taskState[i]==1)
       {
         document.getElementsByClassName("time")[i].innerHTML=( sec[i] > 58)? parseInt(++sec[i]/60,10)+ " mins "+pad(sec[i]%60)+" secs": pad(++sec[i]%60)+" secs";
